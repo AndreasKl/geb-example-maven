b73a43843a3546d33ec12c04587633af0caa5c12
Andreas Kluth <AndreasKl@users.noreply.github.com>
2017-06-27 22:05:27 +0200

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,37 +1,9 @@
-/*
-	This is the Geb configuration file.
-	
-	See: http://www.gebish.org/manual/current/configuration.html
-*/
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeDriverService
+import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.os.CommandLine
 
 import static org.apache.commons.lang3.SystemUtils.IS_OS_LINUX
 import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC
 
-File findDriverExecutable() {
-    def defaultExecutable = CommandLine.find("chromedriver")
-    if (defaultExecutable) {
-        new File(defaultExecutable)
-    } else {
-        new File("drivers").listFiles().findAll {
-            !it.name.endsWith(".version")
-        }.find {
-            if (IS_OS_LINUX) {
-                it.name.contains("linux")
-            } else if (IS_OS_MAC) {
-                it.name.contains("mac")
-            }
-        }
-    }
-}
-
-driver = {
-    ChromeDriverService.Builder serviceBuilder = new ChromeDriverService.Builder()
-        .usingAnyFreePort()
-        .usingDriverExecutable(findDriverExecutable())
-    new ChromeDriver(serviceBuilder.build())
-}
+driver = { new FirefoxDriver() }
 
 baseUrl = "http://gebish.org"
