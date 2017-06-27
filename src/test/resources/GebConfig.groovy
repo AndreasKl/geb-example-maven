d1739a341e5589cb310c5c43b0d9d7fb92bf62c7
Andreas Kluth <AndreasKl@users.noreply.github.com>
2017-06-27 23:46:18 +0200

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,9 +1,12 @@
-import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.os.CommandLine
 
 import static org.apache.commons.lang3.SystemUtils.IS_OS_LINUX
 import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC
 
-driver = { new FirefoxDriver() }
+driver = { 
+  System.setProperty('webdriver.chrome.driver', '/usr/lib/chromium/chromedriver')
+  new ChromeDriver() 
+}
 
 baseUrl = "http://gebish.org"
